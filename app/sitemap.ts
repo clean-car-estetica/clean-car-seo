81165c448a46e83d9ff6de5c6b765cca78286c58
Glauber <glauber@gbr-sistemas.tec.br>
2026-07-25 23:34:55 +0000

    Fix: sitemap.xml estava congelado desde o build, sem se atualizar com novos posts do blog. Agora revalida a cada 5 minutos

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -4,6 +4,8 @@ import { getServicosPublicos, getCidadesPublicas } from "@/lib/site-data";
 
 const BASE_URL = "https://clean-car-seo.vercel.app";
 
+export const revalidate = 300;
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const [servicos, cidades] = await Promise.all([getServicosPublicos(), getCidadesPublicas()]);
 
